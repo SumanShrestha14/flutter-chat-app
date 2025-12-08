76f07aa6c6cad31218d4ab407dc563fd8d681b33
Suman Shrestha <sumanstha0614@gmail.com>
2025-12-08 21:35:11 +0545

    09 :: show block dialouge box

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -3,22 +3,61 @@ import 'package:flutter/material.dart';
 class ChatBubble extends StatelessWidget {
   final String message;
   final bool isCurrentUser;
+  final String messageID;
+  final String userID;
   const ChatBubble({
     super.key,
     required this.message,
     required this.isCurrentUser,
+    required this.messageID,
+    required this.userID,
   });
+
+  // Show Options
+  void showOptions(BuildContext context, String messageID, String userID) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SafeArea(
+          child: Wrap(
+            children: [
+              // report message
+              ListTile(
+                leading: const Icon(Icons.flag),
+                title: const Text("Report"),
+                onTap: () {},
+              ),
+              // block user
+              // cancel
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // report message
+
+  // Block user
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(bottom: 2.5, right: 10, left: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: isCurrentUser ? Colors.blue : Colors.grey.shade800,
+    return GestureDetector(
+      onLongPress: () {
+        if (!isCurrentUser) {
+          // show options to report block and unblock
+          showOptions(context, messageID, userID);
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(bottom: 2.5, right: 10, left: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: isCurrentUser ? Colors.blue : Colors.grey.shade800,
+        ),
+        child: Text(message, style: const TextStyle(color: Colors.white)),
       ),
-      child: Text(message, style: const TextStyle(color: Colors.white)),
     );
   }
 }
