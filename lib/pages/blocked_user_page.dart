9d53442c20ef925ba9dbe70891bfe2fe419ce392
Suman Shrestha <sumanstha0614@gmail.com>
2025-12-09 23:04:24 +0545

    09 :: Unblock management

--- lib/pages/blocked_user_page.dart
+++ lib/pages/blocked_user_page.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/components/user_tile.dart';
 import 'package:flutter_chat_app/features/auth/auth_service.dart';
 import 'package:flutter_chat_app/features/chat/chat_services.dart';
 
-class BlockedUserPage extends StatefulWidget {
-  const BlockedUserPage({super.key});
-
-  @override
-  State<BlockedUserPage> createState() => _BlockedUserPageState();
-}
-
-class _BlockedUserPageState extends State<BlockedUserPage> {
+class BlockedUserPage extends StatelessWidget {
+  BlockedUserPage({super.key});
   // chat and auth service
   final AuthService authService = AuthService();
   final ChatServices chatServices = ChatServices();
-  String userID = AuthService().getCurrentUser()!.uid;
 
   @override
   Widget build(BuildContext context) {
+    String userID = AuthService().getCurrentUser()!.uid;
     return Scaffold(
       appBar: AppBar(title: Text("Blocked Users")),
       body: StreamBuilder<List<Map<String, dynamic>>>(
-        stream: chatServices.getBlockedUserStream(auth.getCurrentUser()),
-        builder: builder,
+        stream: chatServices.getBlockedUserStream(userID),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text("Error occured"));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text("Loading ..."));
+          }
+
+          final blockedUser = snapshot.data ?? [];
+          // if no users
+          if (blockedUser.isEmpty) {
+            return const Center(child: Text("Block list is empty.."));
+          }
+
+          return ListView.builder(
+            itemCount: blockedUser.length,
+            itemBuilder: (context, index) {
+              final user = blockedUser[index];
+              return UserTile(
+                text: user["email"],
+                onTap: () => showUnblockBox(context, user["uid"]),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void showUnblockBox(BuildContext context, String userID) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Unblock user"),
+        content: const Text("Are you sure you want to unblock this user"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              chatServices.unblockUser(userID);
+              Navigator.pop(context);
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Unblock Successful")),
+              );
+            },
+            child: const Text("Unblock"),
+          ),
+        ], // cancel button
       ),
     );
   }
